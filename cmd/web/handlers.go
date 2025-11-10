0c934706a5523c9f85d7c70b9aecb63b5c3d8341
Md. Talim <talimmohammad116@gmail.com>
2025-11-10 22:21:18 +0530

    fix: update key spelling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -194,7 +194,7 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.sessionManager.Put(r.Context(), "authenticateUserID", id)
+	app.sessionManager.Put(r.Context(), "authenticatedUserID", id)
 
 	http.Redirect(w, r, "/snippet/create", http.StatusSeeOther)
 }
